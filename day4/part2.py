3a7cff39af3f565f3e881ba3be17cd7da68e1196
mindcrackx <mindcrackx@protonmail.com>
2022-12-04 06:38:05 +0100

    add day4

---
+++ day4/part2.py
@@ -0,0 +1,27 @@
+data = open("input.txt", "r", encoding="utf-8").read().splitlines()
+
+
+def test_input():
+    return """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""".splitlines()
+
+
+# data = test_input()
+
+
+count = 0
+for line in data:
+    left, right = line.split(",")
+    a, b = left.split("-")
+    c, d = right.split("-")
+    # got stuck ~ 16 minutes until i realized i forgot to convert to ints...
+    a, b, c, d = int(a), int(b), int(c), int(d)
+
+    if a <= d and b >= c:
+        count += 1
+
+print(count)
